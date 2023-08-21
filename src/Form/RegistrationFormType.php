a3db273246dbb3ab41a668a36fe4c946e4a40dc6
schevallier2023 <s.chevallier@hotmail.fr>
2023-08-21 14:23:29 +0200

    modification du form register et add de ville dans la bd de l'entité Site

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,10 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Site;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,33 +21,45 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('pseudo',null,["label"=>"Pseudo : "])
+            ->add('nom',null,["label"=>"Nom : "])
+            ->add('prenom',null,["label"=>"Prénom : "])
+            ->add('telephone',null,["label"=>"Téléphone : "])
+            ->add('email',null,["label"=>"Email : "])
+            ->add('siteEni', EntityType::class,
+            ['class' => Site::class,
+                'choice_label' =>'nom',
+                'label'=>'Site de l\'ENI : '
+            ])
+        ->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Le mots de passe doit être le même.',
+            'options' => ['attr' => ['class' => 'password-field']],
+            'required' => true,
+            'first_options' => ['label' => 'Mots de passe '],
+            'second_options' => ['label' => 'Répeter le mots de passe '],
+            'mapped' => false,
+            'attr' => ['autocomplete' => 'new-password'],
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Merci d\'entrer le mots de passe',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Le mots de passe doit faire moins de {{ limit }} charactères',
+                    'max' => 4096,
+                ]),
+            ],
+        ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Merci d\'accepter les thermes.',
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+            ->add('ajouter', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void

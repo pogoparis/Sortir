379435614074cd6c9cdedcdea710b10ab629c2a3
schevallier2023 <s.chevallier@hotmail.fr>
2023-08-23 10:13:18 +0200

    suppr du Assert password

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -32,7 +32,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
-    #[Assert\NotBlank]
+
     private ?string $password = null;
 
     #[ORM\Column(length: 40)]

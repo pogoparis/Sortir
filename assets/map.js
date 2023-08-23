61cfaa1898beca0eb255f0abd8fba330257d77c8
julienM29 <miossec.julien@orange.fr>
2023-08-23 10:17:50 +0200

    filtre javascript :clic sur la ville -> lieux associés

--- assets/map.js
+++ assets/map.js
@@ -2,58 +2,64 @@ window.onload = init;
 
 let marker;
 function init() {
-    map = L.map('map').setView([51.505, -0.09], 3);
+    map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}\'', {
         maxZoom: 19,
         attribution: '© OpenStreetMap'
     }).addTo(map);
-    setInterval(coordonnee, 1000);
     affichageVille();
-    allLieux();
 }
 function afficherLieu(){
      $id = document.getElementById("selectVille").value;
 
 console.log($id);
-    fetch('http://127.0.0.1:8000/apiLieux')
-        .then(res => res.json())
-        .then(
-            json => {
-                for (const js of json){
-                    let i =0;
-                    $idVille = js["ville"]["id"];
+    if($id !== 0){
 
-                    if (+$idVille == $id){
-                        console.log('ça marche')
-                        document.getElementById("selectLieux").hidden = false;
-                        $i = js["id"];
-                        $lieu =  document.getElementById("selectLieux").options[$i];
-                        $lieu.hidden = true;
+        fetch(`http://127.0.0.1:8000/apiLieux/${$id}`)
+            .then(res => res.json())
+            .then(
+             json => {
+                    for (const js of json){
+                        $idVille = js["ville"]["id"];
+                        $idVilleInt = parseInt($idVille);
+                       console.log('idville' + typeof $idVille);
+                        console.log('idvilleint' + typeof $idVilleInt);
+                        if ($idVille !== $idVilleInt){
+                            console.log('ça marche')
+                            document.getElementById("selectLieux").hidden = false;
+                         $i = js["id"];
+                         $lieu =  document.getElementById("selectLieux").options[$i];
+                         $lieu.hidden = true;
+                        }
+
                     }
-
-                }
-            }
-        )
+             }
+        )}
 }
 window.afficherLieu = afficherLieu;
 function allLieux() {
-    fetch('http://127.0.0.1:8000/apiLieux')
+
+    $id = document.getElementById("selectVille").value;
+    let select = document.getElementById("selectLieux");
+        select.innerText = '';
+    let nouvelElement = document.createElement("option");
+    // nouvelElement.setAttribute("value", '0');
+    // nouvelElement.innerText = 'choisir un lieu';
+    // select.appendChild(nouvelElement);
+    if($id !== 0){
+        fetch(`http://127.0.0.1:8000/apiLieux/${$id}`)
         .then(res => res.json())
         .then(
             json => {
-                let select = document.getElementById("selectLieux");
-                select.hidden = true;
 
                 for (const js of json){
-
-                    let nouvelElement = document.createElement("option");
 
                     nouvelElement.setAttribute("value", js['id'])
                     nouvelElement.innerText = js['nom'];
                     select.appendChild(nouvelElement);
                 }
             }
-        )
+        )}
 }
 function affichageVille() {
     fetch('http://127.0.0.1:8000/api')
@@ -69,7 +75,7 @@ function affichageVille() {
                     nouvelElement.setAttribute("value", js['id'])
                     nouvelElement.innerText = js['nom'];
                     select.appendChild(nouvelElement);
-                    document.getElementById("selectVille").addEventListener("click", afficherLieu);
+                    document.getElementById("selectVille").addEventListener("click", allLieux);
                 }
             }
         )

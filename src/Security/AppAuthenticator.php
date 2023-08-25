f635f5023daeeb4f7d6f25e579f556f1a4c50338
schevallier2023 <s.chevallier@hotmail.fr>
2023-08-25 16:34:49 +0200

    modif de la route après connection envoi sur l'afficher sortie

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -49,7 +49,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-         return new RedirectResponse($this->urlGenerator->generate('main_index'));
+         return new RedirectResponse($this->urlGenerator->generate('sortie_affichage'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
